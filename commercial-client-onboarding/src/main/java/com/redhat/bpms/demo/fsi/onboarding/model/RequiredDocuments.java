58fa7f4a22076c6ef380d1ccc357b6a72f4df73d
bpmsAdmin <>
2017-10-11 15:12:10 +0000

    {/commercial-client-onboarding/src/main/java/com/redhat/bpms/demo/fsi/onboarding/model/RequiredDocuments.java}

--- commercial-client-onboarding/src/main/java/com/redhat/bpms/demo/fsi/onboarding/model/RequiredDocuments.java
+++ commercial-client-onboarding/src/main/java/com/redhat/bpms/demo/fsi/onboarding/model/RequiredDocuments.java
@@ -10,7 +10,7 @@ public class RequiredDocuments implements java.io.Serializable
    static final long serialVersionUID = 1L;
 
    @org.kie.api.definition.type.Label(value = "Required Documents")
-   private java.util.List<java.lang.String> requiredDocs;
+   private java.util.List<java.lang.String> requiredDocs = new java.util.ArrayList<>();
 
    public RequiredDocuments()
    {
@@ -29,6 +29,10 @@ public class RequiredDocuments implements java.io.Serializable
    public RequiredDocuments(java.util.List<java.lang.String> requiredDocs)
    {
       this.requiredDocs = requiredDocs;
+   }
+   
+   public void addRequiredDocument(String requiredDoc) {
+       requiredDocs.add(requiredDoc);
    }
 
 }
